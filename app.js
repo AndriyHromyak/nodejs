f9e908c33ffb421b39bf8e2af867dfbc1300176d
andriy_hromyak <andriy_hromyak@epam.com>
2019-11-20 20:06:35 +0200

    11

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoClient = new MongoClient("mongodb://0.0.0.0:27017/", { useNewUrlParse
  
 let dbClient;
 
-console.log(10);
+console.log(11);
  
 app.use(express.static(__dirname + "/public"));
  
